2d83582634d4463876f3ffecc9b20517570e33c3
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2026-05-31 03:06:08 +0200

    user controller return fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,12 +10,12 @@ export const updateUserAvatar = async (req, res) => {
 
   const result = await saveFileToCloudinary(file.buffer, user._id);
 
-  await User.findOneAndUpdate(
-    { _id: user._id },
+  const updatedUser = await User.findOneAndUpdate(
+    { id: user._id },
     { avatar: result.secure_url },
     { returnDocument: 'after' }
   );
 
 
-  res.status(200).json({ url: user.avatar });
+  res.status(200).json({ url: updatedUser.avatar });
 };
